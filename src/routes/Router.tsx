0a34b8e92f35d733313f05905cccac2dd8fead87
Nitesh Pant <niteshp@capitalnumbers.com>
2025-04-26 10:09:24 +0530

    Completed take-home assignment

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes, Navigate } from "react-router-dom";
 import Home from "../pages/Home.tsx";
 import Login from "../pages/Login.tsx";
 import PrivateRoute from "../components/PrivateRoute.tsx";
@@ -7,13 +7,19 @@ import ForgotPassword from "../pages/ForgotPassowrd.tsx";
 import Signup from "../pages/Signup.tsx";
 
 const AppRouter: React.FC = () => {
+  const isLoggedIn = !!localStorage.getItem("user");
+
   return (
     <Router>
       <Routes>
+        {/* Navigate to login or home */}
+        <Route path="/" element={<Navigate to={isLoggedIn ? "/home" : "/login"} replace />}/>
+
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/signup" element={<Signup />} />
 
+        {/* private route */}
         <Route
           path="/home"
           element={
